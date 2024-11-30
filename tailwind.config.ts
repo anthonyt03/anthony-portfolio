c27024bf150e41bf15e9654d0afeef2808219b98
Anthony Tran <72509108+anthonyt03@users.noreply.github.com>
2024-11-30 13:29:36 -0500

    install shadcn and aceternity ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,41 @@
-import { type Config } from 'tailwindcss'
-import { fontFamily } from 'tailwindcss/defaultTheme'
+import type { Config } from 'tailwindcss'
 import tailwindcssAnimate from 'tailwindcss-animate'
 
-export default {
+const config = {
   darkMode: ['class'],
-  content: ['./src/**/*.tsx'],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
-      fontFamily: {
-        sans: ['var(--font-geist-sans)', ...fontFamily.sans],
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
       },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
-      colors: {},
     },
   },
   plugins: [tailwindcssAnimate],
 } satisfies Config
+
+export default config
